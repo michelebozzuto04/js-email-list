ec68bfa6bd76fb3bbe7be4e2238863b8b576b03c
michelebozzuto04 <michele.bozzuto04@gmail.com>
2025-06-16 15:34:38 +0200

    refactor: implement email fetching in a loop and store results in an array

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
-fetch('https://flynn.boolean.careers/exercises/api/random/mail')
-    .then(response => response.json())
-    .then(data => {
-        console.log(data);
-    })
-    .catch(error => {
-        console.error(error);
-    });
+const emails = [];
+
+for (let i = 0; i < 10; i++) {
+    fetch('https://flynn.boolean.careers/exercises/api/random/mail')
+        .then(response => response.json())
+        .then(data => {
+            const email = data.response;
+            emails.push(email);
+        })
+        .catch(error => {
+            console.error(error);
+        });
+}
+
+console.log(emails);
