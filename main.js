6df6221d3dea782d7abd31d553d1d54295579671
michelebozzuto04 <michele.bozzuto04@gmail.com>
2025-06-16 15:28:08 +0200

    implement fetch request to retrieve random email and log response

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-console.log("Ciao Prova")
+fetch('https://flynn.boolean.careers/exercises/api/random/mail')
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+    })
+    .catch(error => {
+        console.error(error);
+    });
