a9376ca9f05b02b51b6f0fea272b5b0afd065fb4
michelebozzuto04 <michele.bozzuto04@gmail.com>
2025-06-16 15:55:07 +0200

    refactor: move email fetching logic into a function and update button event listener

--- main.js
+++ main.js
@@ -1,19 +1,28 @@
 const listEl = document.getElementById("list")
+const buttonEl = document.getElementById("button")
 const emails = [];
 
-for (let i = 0; i < 10; i++) {
-    fetch('https://flynn.boolean.careers/exercises/api/random/mail')
-        .then(response => response.json())
-        .then(data => {
-            const email = data.response;
-            emails.push(email);
-            liEl = document.createElement('li');
-            liEl.innerText = data.response;
-            listEl.appendChild(liEl);
-        })
-        .catch(error => {
-            console.error(error);
-        });
+function fetchEmails(array, length) {
+    for (let i = 0; i < length; i++) {
+        fetch('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then(response => response.json())
+            .then(data => {
+                const email = data.response;
+                array.push(email);
+                liEl = document.createElement('li');
+                liEl.innerText = data.response;
+                listEl.appendChild(liEl);
+            })
+            .catch(error => {
+                console.error(error);
+            });
+    }
 }
 
-console.log(emails);
+fetchEmails(emails, 10)
+
+buttonEl.addEventListener('click', () => {
+    emails.length = 0;
+    listEl.innerHTML = '';
+    fetchEmails(emails, 10)
+})
